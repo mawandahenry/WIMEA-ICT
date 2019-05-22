132f3a78943272f134f5b6ba07548f8412b4dbce
Mawanda Henry <mawandahenrynaco@gmail.com>
2019-05-22 15:41:23 +0300

    update version 2.0.2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-
-  function connect(div1, div2, color, thickness) {
+  var __dimensions = {};
+  function connect(div1, div2) {
       var off1 = getOffset(div1);
       var off2 = getOffset(div2);
       // bottom right
@@ -10,17 +10,10 @@
       var y2 = off2.top+off2.height/2;
       // distance
       var length = Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
-      // center
-      var cx = ((x1 + x2) / 2) - (length / 2);
-      var cy = ((y1 + y2) / 2) - (thickness / 2);
-      // angle
-      var angle = Math.atan2((y1-y2),(x1-x2))*(180/Math.PI);
-      // make hr
-      var htmlLine = "<div style='padding:0px; margin:0px; height:" + thickness + "px; background-color:" + color + "; line-height:1px; position:absolute; left:" + cx + "px; top:" + cy + "px; width:" + length + "px; -moz-transform:rotate(" + angle + "deg); -webkit-transform:rotate(" + angle + "deg); -o-transform:rotate(" + angle + "deg); -ms-transform:rotate(" + angle + "deg); transform:rotate(" + angle + "deg);' />";
-      //
-      console.log(off2);
-      console.log(x2);
-      document.body.innerHTML += htmlLine;
+
+      console.log(length);
+      //console.log(x2);
+      //document.body.innerHTML += htmlLine;
   }
 
   function getOffset( el ) {
@@ -66,31 +59,65 @@
   var x = tagObj[0].attributes[0];
   for(var i = 0; i<tagObj.length; i++){
     arr.push(tagObj[i].attributes[0].nodeValue);
-    var first = tagObj[i].attributes[4].nodeValue.replace(/['"]+/g, '');
-    tag += "<tr><td><img id='drag1' class = 'opo' src ='"+tagObj[i].attributes[0].nodeValue+"' width='50' height='50' draggable = 'true' ondragstart='drag(event)'></td><td>"+first+"</td></tr>";
+    var first = tagObj[i].attributes[2].nodeValue.replace(/['"]+/g, '');
+    tag += "<tr><td><img id='drag1' class = 'opo' src ='"+tagObj[i].attributes[0].nodeValue+"' is_main = "+tagObj[i].attributes[3].nodeValue+" width='50' height='50' ></td><td>"+first+"</td></tr>";
+    console.log(tagObj[i])
 
   }
   document.getElementById('ban').innerHTML = tag;
 
   }
 
-  function allowDrop(ev) {
-    ev.preventDefault();
-  }
-  function drag(ev) {
-  ev.dataTransfer.setDragImage("text", ev.target.id);
+
+
+var mnet = document.getElementById('#bus');
+// = getOffset(mnet);
+console.log(getOffset(mnet));
+
+function collision($div1, $div2) {
+        var x1 = $div1.offset().left;
+        var y1 = $div1.offset().top;
+        var z1 = $div1.offset().bottom;
+        var a1 = $div1.offset().right;
+        var h1 = $div1.outerHeight(true);
+        var w1 = $div1.outerWidth(true);
+        var b1 = y1 + h1;
+        var r1 = x1 + w1;
+        var r2 = a1+w1;
+
+        var x2 = $div2.offset().left;
+        var y2 = $div2.offset().top;
+        var h2 = $div2.outerHeight(true);
+        var w2 = $div2.outerWidth(true);
+        var z2 = $div1.offset().bottom;
+        var a2 = $div1.offset().right;
+        var b2 = y2 + h2;
+        var r2 = x2 + w2;
+
+        if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2|| h1<h2) return false;
+        return true;
 }
-function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
+function setDimensions($place1, $place2){
+  var x1 = $place1.offset().left;
+  var y1 = $place1.offset().top;
+  var h1 = $place1.outerHeight(true);
+  var w1 = $place1.outerWidth(true);
+
+  var x2 = $place2.offset().left;
+  var y2 = $place2.offset().top;
+  var h2 = $place2.outerHeight(true);
+  var w2 = $place2.outerWidth(true);
+
+  x1 = x2;
+  y1 = y2;
+  h1 = h2;
+  w1 = w2;
 }
-function locatePointer(e){
-  var e = window.event;
-  var posX = e.clientX;
-  var posY = e.clientY;
-  console.log(posX);
+
+function createDiv($number){
+  var counter = 0;
+
+  var tx = "<div id = 'num"+counter+"' class = 'beat'></div>";
+  counter++;
+  return tx;
 }
-var mnet = document.getElementById('bus');
-var offf = getOffset(mnet);
-console.log(offf);
