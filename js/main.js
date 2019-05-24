b5b03a0d1b30b85d8f047d8e13387591400d8ea4
Mawanda Henry <mawandahenrynaco@gmail.com>
2019-05-24 08:19:57 +0300

    update version 3.0.1

--- js/main.js
+++ js/main.js
@@ -1,19 +1,51 @@
-  var __dimensions = {};
-  function connect(div1, div2) {
+
+function connectHorizontal(div1, div2, color, thickness) {
+    var off_1 = getOffset(div1);
+    var off_2 = getOffset(div2);
+    console.log(off_1);
+    console.log(off_2);
+    var x1 =  off_1.left+off_1.width;
+    var y1 = off_1.left;
+    var x2 = off_2.left;
+    var y2 = off_2.left+off_2.height/2;
+    var length = Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
+
+    var cx = ((x1 + x2) / 2) - (length / 2);
+    var cy = ((y1 + y2) / 2) - (thickness / 2);
+    var angle = Math.atan2((y1-y2),(x1-x2))*(180/Math.PI);
+    var htmlLine = "<div style='padding:0px; margin:0px; height:" + thickness + "px; background-color:" + color + "; line-height:1px; position:absolute; left:" + cx + "px; top:" + cy + "px; width:" + length + "px; -moz-transform:rotate(" + angle + "deg); -webkit-transform:rotate(" + angle + "deg); -o-transform:rotate(" + angle + "deg); -ms-transform:rotate(" + angle + "deg); transform:rotate(" + angle + "deg);' />";
+    document.body.innerHTML += htmlLine;
+}
+
+  function connect(div1, div2, color, thickness) {
       var off1 = getOffset(div1);
       var off2 = getOffset(div2);
-      // bottom right
       var x1 = off1.left + off1.width;
       var y1 = off1.top + off1.height/2;
-      // top right
       var x2 = off2.left;
       var y2 = off2.top+off2.height/2;
-      // distance
       var length = Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
+      var cx = ((x1 + x2) / 2) - (length / 2);
+      var cy = ((y1 + y2) / 2) - (thickness / 2);
+      var angle = Math.atan2((y1-y2),(x1-x2))*(180/Math.PI);
+      var htmlLine = "<div style='padding:0px; margin:0px; height:" + thickness + "px; background-color:" + color + "; line-height:1px; position:absolute; left:" + cx + "px; top:" + cy + "px; width:" + length + "px; -moz-transform:rotate(" + angle + "deg); -webkit-transform:rotate(" + angle + "deg); -o-transform:rotate(" + angle + "deg); -ms-transform:rotate(" + angle + "deg); transform:rotate(" + angle + "deg);' />";
+      document.body.innerHTML += htmlLine;
+  }
 
-      console.log(length);
-      //console.log(x2);
-      //document.body.innerHTML += htmlLine;
+  function connectVertical(div1, div2, color, thickness) {
+      var off1 = getOffset(div1);
+      var off2 = getOffset(div2);
+
+      var x1 =  off1.left+off1.width/2;
+      var y1 = off1.top+off1.height;
+      var x2 = off2.left+ off2.width/2;
+      var y2 = off2.top;
+      var length = Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
+      var cx = ((x1 + x2) / 2) - (length / 2);
+      var cy = ((y1 + y2) / 2) - (thickness / 2);
+      var angle = Math.atan2((y1-y2),(x1-x2))*(180/Math.PI);
+      var htmlLine = "<div style='padding:0px; margin:0px; height:" + thickness + "px; background-color:" + color + "; line-height:1px; position:absolute; left:" + cx + "px; top:" + cy + "px; width:" + length + "px; -moz-transform:rotate(" + angle + "deg); -webkit-transform:rotate(" + angle + "deg); -o-transform:rotate(" + angle + "deg); -ms-transform:rotate(" + angle + "deg); transform:rotate(" + angle + "deg);' />";
+      document.body.innerHTML += htmlLine;
   }
 
   function getOffset( el ) {
@@ -29,12 +61,6 @@
       return { top: _y, left: _x, width: _w, height: _h };
   }
 
-  window.testIt = function() {
-      var div1 = document.getElementById('div1');
-      var div2 = document.getElementById('div2')
-      connect(div1, div2, "#0F0", 2);
-
-  }
   function readXml(xmlFile){
 
   var xmlDoc;
@@ -61,7 +87,7 @@
     arr.push(tagObj[i].attributes[0].nodeValue);
     var first = tagObj[i].attributes[2].nodeValue.replace(/['"]+/g, '');
     tag += "<tr><td><img id='drag1' class = 'opo' src ='"+tagObj[i].attributes[0].nodeValue+"' is_main = "+tagObj[i].attributes[3].nodeValue+" width='50' height='50' ></td><td>"+first+"</td></tr>";
-    console.log(tagObj[i])
+    // console.log(tagObj[i])
 
   }
   document.getElementById('ban').innerHTML = tag;
@@ -70,31 +96,33 @@
 
 
 
-var mnet = document.getElementById('#bus');
-// = getOffset(mnet);
-console.log(getOffset(mnet));
+
+//var mnet2 = document.getElementById('i2c');
+
 
 function collision($div1, $div2) {
         var x1 = $div1.offset().left;
         var y1 = $div1.offset().top;
-        var z1 = $div1.offset().bottom;
-        var a1 = $div1.offset().right;
+
+
         var h1 = $div1.outerHeight(true);
         var w1 = $div1.outerWidth(true);
         var b1 = y1 + h1;
         var r1 = x1 + w1;
-        var r2 = a1+w1;
+
+
 
         var x2 = $div2.offset().left;
         var y2 = $div2.offset().top;
         var h2 = $div2.outerHeight(true);
         var w2 = $div2.outerWidth(true);
-        var z2 = $div1.offset().bottom;
-        var a2 = $div1.offset().right;
-        var b2 = y2 + h2;
+
+
+
+        var b2 = y2 + h2;//bottom
         var r2 = x2 + w2;
 
-        if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2|| h1<h2) return false;
+        if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2 ) return false;
         return true;
 }
 function setDimensions($place1, $place2){
@@ -103,21 +131,18 @@ function setDimensions($place1, $place2){
   var h1 = $place1.outerHeight(true);
   var w1 = $place1.outerWidth(true);
 
-  var x2 = $place2.offset().left;
-  var y2 = $place2.offset().top;
-  var h2 = $place2.outerHeight(true);
-  var w2 = $place2.outerWidth(true);
+  $place2.offset().left = x1;
+  $place2.offset().top = y1;
+  //$place2.outerHeight() = h1;
+  //$place2.outerWidth() = w1;
 
-  x1 = x2;
-  y1 = y2;
-  h1 = h2;
-  w1 = w2;
+return $place2;
 }
 
-function createDiv($number){
+function createDiv(){
   var counter = 0;
 
-  var tx = "<div id = 'num"+counter+"' class = 'beat'></div>";
-  counter++;
+  var tx = "<svg class='dragie' width='500' height='500'><line x1='50' y1='50' x2='350' y2='350' stroke='black'/></svg>"
+
   return tx;
 }
